e670d945314f7cb083fb6fc7a9460ba138db975e
cbrxyz <main@cbrown.xyz>
2019-11-25 21:57:08 -0500

    Tried to use log; doesn't appear to be working

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -37,7 +37,7 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
-    //log.info("The robot is alive!");
+    log.info("The robot is alive!");
   }
 
   /**
