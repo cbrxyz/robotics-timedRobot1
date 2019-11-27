a5289fb16372e1edafb10386a7cb071b84f830e0
Cameron Brown <cameronbrown@Camerons-MacBook-Air-2.local>
2019-11-27 17:58:40 -0500

    Added more complexity to Drive subsystem

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -22,11 +22,33 @@ public class Drive extends Subsystem {
   TalonSRX rightTalon = new TalonSRX(0);
   TalonSRX leftTalon = new TalonSRX(1);
 
+  TalonSRX rightTalon2 = new TalonSRX(2);
+  TalonSRX leftTalon2 = new TalonSRX(3);
+
+  TalonSRX rightTalon3 = new TalonSRX(4);
+  TalonSRX leftTalon3 = new TalonSRX(5);
+
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
     rightTalon.setOutput(ControlMode.PercentOutput, 0);
     leftTalon.setOutput(ControlMode.PercentOutput, 0);
+
+    rightTalon2.follow(rightTalon);
+    rightTalon3.follow(rightTalon);
+    leftTalon2.follow(leftTalon);
+    leftTalon3.follow(leftTalon);
+
+    //In the case it is needed, use the setInverted commands below to actually have the motors spin the right way
+    /**
+     * rightTalon.setInverted(true);
+     * leftTalon.setInverted(true);
+     * 
+     * rightTalon2.setInverted(InvertedType.FollowMaster);
+     * rightTalon3.setInverted(InvertedType.FollowMaster);
+     * leftTalon2.setInverted(InvertedType.FollowMaster);
+     * leftTalon3.setInverted(InvertedType.FollowMaster);
+     */
   }
 }
