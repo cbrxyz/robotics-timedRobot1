1ba6daf989e2e2b419236148c969644869c9050b
Cameron Brown <cameronbrown@Camerons-MacBook-Air-2.local>
2019-11-27 17:37:58 -0500

    Added TalonSRX instances

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -19,9 +19,14 @@ public class Drive extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
+  TalonSRX rightTalon = new TalonSRX(0);
+  TalonSRX leftTalon = new TalonSRX(1);
+
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
+    rightTalon.setOutput(ControlMode.PercentOutput, 0);
+    leftTalon.setOutput(ControlMode.PercentOutput, 0);
   }
 }
